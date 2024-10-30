12b12c6e43b47ff3392b5f28e2772bc0bcce474c
Mario Muñoz <j.mario.msx@gmail.com>
2024-10-30 13:56:56 +0100

    added tests

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,7 +1,4 @@
 import '@testing-library/jest-dom';
 import { TextEncoder, TextDecoder } from 'util';
-import ResizeObserver from 'resize-observer-polyfill';
 
 Object.assign(global, { TextDecoder, TextEncoder });
-
-global.ResizeObserver = ResizeObserver;
