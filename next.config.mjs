fbe8529b458b6ee8054dddfdc27cdb0f06abccc2
Mario Muñoz <j.mario.msx@gmail.com>
2024-11-07 13:31:48 +0100

    modified next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: [
-    'next-auth',
-    '@auth/core',
-    '@panva/hkdf',
-    'jose',
-    'oauth4webapi',
-    'preact',
-    'preact-render-to-string',
-  ],
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
 };
 
 export default nextConfig;
